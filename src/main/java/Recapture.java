e616a5ff2bb2dd97564c856ca81e0b6e400f5869
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-22 15:21:58 +0300

    idk

--- src/main/java/Recapture.java
+++ src/main/java/Recapture.java
@@ -1,7 +1,10 @@
 import arc.Events;
+import arc.util.Log;
 import arc.util.Timer;
+import mindustry.Vars;
 import mindustry.entities.Units;
 import mindustry.game.EventType;
+import mindustry.game.Rules;
 import mindustry.game.Team;
 import mindustry.mod.Plugin;
 import mindustry.world.blocks.storage.*;
@@ -15,8 +18,9 @@ public class Recapture extends Plugin {
                 var team = e.tile.build.team;
                 var block = e.tile.build.block;
                 Timer.schedule(() -> {
-                    var closestEnemy = Units.closestEnemy(team, tile.worldx(), tile.worldy(), 64 * block.size, u -> true);
+                    var closestEnemy = Units.closestEnemy(team, tile.worldx(), tile.worldy(), Vars.state.rules.enemyCoreBuildRadius, u -> true);
                     tile.setBlock(block, closestEnemy != null ? closestEnemy.team : Team.derelict);
+                    tile.recache();
                 }, 0.1f);
             }
         });
